427cdd653dc973af6b01770793a66b554563c86e
Finaritra Mario Rakotonindrina <finarako@c1r1s3.42antananarivo.mg>
2026-06-14 18:17:59 +0300

    firs module 2 commit

---
+++ ex0/ft_first_exception.py
@@ -0,0 +1,37 @@
+def input_temperature(temp_str: str) -> int:
+    temp_int = int(temp_str)
+    if (temp_int < 0):
+        print("Caught input_temperature error: "
+              f"{temp_int}°C is too cold for plants (min 0°C)")
+    elif (temp_int > 40):
+        print()
+    else:
+        return temp_int
+
+
+def test_temperature():
+    value = "25"
+    print(f"Input data is '{value}'")
+
+    input_temperature(value)
+    print(f"Temperature is now {value}°C")
+    print()
+    try:
+        value = "abc"
+        print(f"Input data is '{value}'")
+
+        input_temperature(value)
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+    finally:
+        print("All tests completed - program didn't crash!")
+
+
+def main() -> None:
+    print("=== Garden Temperature ===")
+    print()
+    test_temperature()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ex1/ft_raise_exception.py
@@ -0,0 +1,32 @@
+def input_temperature(temp_str: str) -> int:
+    temp_int = int(temp_str)
+    if (temp_int < 0):
+        raise ValueError(f"{temp_int}°C is too cold for plants (min 0°C)")
+    elif (temp_int > 40):
+        raise ValueError(f"{temp_int}°C is too hot for plants (max 40°C)")
+    else:
+        return temp_int
+
+
+def test_temperature() -> None:
+    value = ["25", "abc", "-50", "100"]
+    for n in value:
+        print(f"input data is '{n}")
+        try:
+            new_value = input_temperature(n)
+            print(f"Temperature is now{new_value}")
+            print()
+        except ValueError as e:
+            print(f"Caught input_temperature error: {e}")
+            print()
+
+
+def main() -> None:
+    print("=== Garden Temperature Checker ===")
+    print()
+    test_temperature()
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    main()
